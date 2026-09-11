fce3b24d5de574151f89cb2caeb4fa8602f0a910
pythonunique <v.yazdani79@gmail.com>
2026-09-11 19:01:47 +0330

    make data from api

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react';
 import {  useLocation } from 'react-router'
 
@@ -8,18 +8,48 @@ export default function Weather() {
   const lng = location.state.lng;
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
   const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+  const [update , setUpdate] = useState(null);
 
+  const [data , setData] = useState([]);
 
   const sendRequest = async()=>{
     const request = await fetch(API);
-    const data = await request.json();
-    console.log(data);
+    const data2 = await request.json();
+    getData(data2)
   }
+
+
+  const getData =  (res) => {
+    const isNight = res.weather[0].icon.endsWith("n");
+    setUpdate({
+      city:res.name,
+      feelsLike:res.main.feels_like,
+      humidity:res.main.humidity,
+      wind:res.wind.speed,
+      temp:res.main.temp,
+      descripton:res.weather[0].description,
+      icon:`https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`,
+      isNight,
+    })
+  };
 
   useEffect(()=>{
     sendRequest();
   },[])
+
+    if(!update){
+      return (
+        <>
+        <p>data is not ready</p>
+        </>
+      )
+    }
+
+
+
   return (
-    <p>vahid yazdani</p>
+    <>
+    <p className='h-15 w-50 bg-green-600'></p>
+    </>
   )
 }
