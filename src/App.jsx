dfda3835a0bffc6356aa94c85804e48023cc9910
pythonunique <v.yazdani79@gmail.com>
2026-09-11 14:31:27 +0330

    add map with leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import Map from "./Map"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <Map />
     </>
   )
 }
---
+++ src/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MapContainer , TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function Map() {
+  const position = [32.4, 53.7]
+  return (
+    <>
+    <MapContainer className='h-screen w-full' center={position} zoom={5}>
+          <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    </MapContainer>
+    </>
+  )
+}
